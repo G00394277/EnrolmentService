709d01bdb1e645937a753b00de3a124709cc2b9b
G00394277 <G00394277@atu.ie>
2024-10-21 13:08:58 +0100

    Initial commit

---
+++ src/main/java/ie/atu/enrolmentapp/EnrolmentAppApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.enrolmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrolmentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnrolmentAppApplication.class, args);
+    }
+
+}
